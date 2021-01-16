9d850551f3eb6c231620762622f27962433191d4
Zeshen <59198316+zzeshen@users.noreply.github.com>
2021-01-16 14:11:09 +0800

    重构仓库

--- multiplier.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-# multiplier.c
-# author @zeshen
-# function - multiplier calculator
-
-x=float(input("请输入底数：\n"))
-y=float(input("请输入指数：\n"))
-print(x**y)
--- readCSV.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy
-c=numpy.recfromcsv(
-    'C:\Program Files (x86)\Python38-32\Lib\site-packages\matplotlib\mpl-data\sample_data\msft.csv',
-  encoding='utf-8' )
-pd.set_option('display.max_columns',1000)
-pd.set_option('display.width',1000)
-pd.set_option('display.max_colwidth',1000)
-print(c)
