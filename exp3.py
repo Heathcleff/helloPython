8765ee8daa039c941403969fe4f540bf8ee512ce
Zeshen <59198316+zzeshen@users.noreply.github.com>
2021-01-16 23:43:20 +0800

    第二次提交

---
+++ exp3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens:")
+
+print("Hens",20+30/6)
+print("Roosters",100-25*3%4)    # 这里的%是取余运算
+
+print("Now I will count the eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+"""
+Python具有数学计算的优先级，乘除法优先于加减法，括号内的式子优先计算
+第一步：3+2+1-5+0-0.25+6
+第二步：6.75
+"""
+
+print(3+2<5-7)
+
+print("What is 3+2?",3+2)
+print("What is 5-7?",5-7)
+
+print("Oh, that's why it's False!")
+
+print("How about some more.")
+
+print("Is it greater?",5>-2)
+print("Is it greater or equal?",5>=-2)
+print("Is ot less or equal?",5<=-2)
