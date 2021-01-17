ec9c96c530cfa8e9e8d89f466676281210b2151e
Zeshen <59198316+zzeshen@users.noreply.github.com>
2021-01-17 17:39:32 +0800

    完成到exp11.py

---
+++ exp11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=" ")
+age = input()
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weight?")
+weight = input()
+
+print(
+    f"So,you're {age} years old,{height} inches tall,and {weight} pound heavy."
+)
